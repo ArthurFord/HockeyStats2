6c61f48c69826ccde7c77c11f201b452b72ba8ca
Arthur Ford <adf.ford@gmail.com>
2022-10-02 07:30:23 -0400

    Initial Commit

---
+++ HockeyStats2/HockeyStats2App.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyStats2App.swift
+//  HockeyStats2
+//
+//  Created by Arthur Ford on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HockeyStats2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
